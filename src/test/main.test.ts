7846968e5b009b145cac3068ea08709b02ebf185
Donald Wu <yeukfei02@gmail.com>
2020-05-13 21:43:49 +0800

    fix-code: update tese case

--- src/test/main.test.ts
+++ src/test/main.test.ts
@@ -1,23 +1,52 @@
 import * as orders from './orders';
+import * as mongoose from 'mongoose';
+import * as express from 'express';
+const app = express();
+
+mongoose.set('useFindAndModify', false);
+
+let dbUrl = '';
+
+const environment = app.get('env');
+if (environment === 'development' || environment === 'test') {
+  // mongo local db
+  dbUrl = 'mongodb://localhost:27017/orders';
+} else {
+  // mongo atlas
+  // dbUrl = `mongodb+srv://yeukfei02:${process.env.MONGO_ATLAS_PASSWORD}@ordersapi-yrvwg.mongodb.net/test?retryWrites=true&w=majority`;
+
+  // docker local mongodb
+  dbUrl = 'mongodb://mongo:27017/orders';
+}
 
 describe('main.test', () => {
+  beforeAll(async () => {
+    await mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+  });
+
   describe('orders', () => {
     test('createOrders', async () => {
       const result = await orders.createOrdersTest();
       console.log('result = ', result);
-      expect(123).toBeDefined();
+      expect(result).toBeDefined();
     });
 
     test('getOrders', async () => {
       const result = await orders.getOrdersTest();
       console.log('result = ', result);
-      expect(123).toBeDefined();
+      expect(result).toBeDefined();
+      expect(result.length > 0).toBeTruthy();
     });
 
     test('updateOrdersById', async () => {
       const result = await orders.updateOrdersByIdTest();
       console.log('result = ', result);
-      expect(123).toBeDefined();
+      expect(result).toBeDefined();
+      expect(result.get('status')).toEqual('TAKEN');
     });
+  });
+
+  afterAll(async () => {
+    await mongoose.disconnect();
   });
 });
