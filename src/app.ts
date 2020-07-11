58a9aca8ac3d6efa8212e676fd05e4233946a737
Donald Wu <yeukfei02@gmail.com>
2020-07-11 11:43:35 +0800

    fix-code: update dockerfile and docker-compose.yml

--- src/app.ts
+++ src/app.ts
@@ -9,13 +9,12 @@ import * as compression from 'compression';
 import * as env from 'dotenv';
 env.config();
 
-import ordersRoutes from './routes/orders';
 import { connectDB } from './db/db';
+
+import ordersRoutes from './routes/orders';
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-connectDB(app);
 
 app.use(cors());
 app.use(helmet());
@@ -23,6 +22,8 @@ app.use(morgan('dev'));
 app.use(compression());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+connectDB(app);
 
 app.use('/api/orders', ordersRoutes);
 
